5a9f078d32d4aed4754abb0c501723767c690028
ondrejoplatka <155572283+ondrejoplatka@users.noreply.github.com>
2024-01-03 19:08:08 +0100

    Added a POST for tasks

--- index.js
+++ index.js
@@ -5,10 +5,21 @@ const port = 3000;
 app.use(express.json());
 
 let tasks = [];
+let nextId = 1;
 
 // GET endpoint to retrieve all tasks
 app.get('/task', (req, res) => {
   res.json(tasks);
+});
+
+// POST endpoint to create a new task
+app.post('/task', (req, res) => {
+  const task = {
+    id: nextId++,
+    content: req.body.content,
+  };
+  tasks.push(task);
+  res.status(201).json(task);
 });
 
 app.get('/', (req, res) => {
