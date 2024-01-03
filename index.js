61a1b8e5e6ed73e7004a72439476647536ef5a17
ondrejoplatka <155572283+ondrejoplatka@users.noreply.github.com>
2024-01-03 19:07:32 +0100

    Added a getter for tasks

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const port = 3000;
 
 app.use(express.json());
 
+let tasks = [];
+
+// GET endpoint to retrieve all tasks
+app.get('/task', (req, res) => {
+  res.json(tasks);
+});
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Todo App!');
 });
